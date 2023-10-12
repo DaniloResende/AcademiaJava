55c912a83c274106697e80adfaa9812cad25c060
Danilo Resende <danilo.rese1@gmail.com>
2023-10-11 21:56:26 -0300

    correcoes

--- JavaWorkSpace/Biblioteca/src/Biblioteca.java
+++ JavaWorkSpace/Biblioteca/src/Biblioteca.java
@@ -1,20 +1,39 @@
 import java.util.ArrayList;
 
 public class Biblioteca {
+    // dica => utilizar metodo  1 .add de arrayList // 2 .get(indice) 3. olhar classe arraylist 
     private ArrayList<Livro> catalogo;
     
 
-    Biblioteca(){
+    public Biblioteca(){
         this.catalogo = new ArrayList<Livro>();
     }
 
-    public void adicionar_livro(Livro livro){
-        catalogo.add(livro);
+    // public boolean adicionar_livro(Livro livro){
+    //     catalogo.add(livro);
+    // }
+    public boolean adicionar_livro(String titulo, String autor, String isbn ){
+    Livro novoLivro = new Livro( titulo,  autor,  isbn );
+    this.catalogo.add(novoLivro);
+    return true;
     }
 
-    public void remover_livro(isbn){
-        for(int i = 0; i < catalogo.size(); i++){
-            if ()
+    public void remover_livro(String isbn){
+        // int tamanhoCatalogo = this.catalogo.size();
+        for(int i = 0; i < this.catalogo.size(); i++){
+            Livro livroAtual = this.catalogo.get(i);
+            if (livroAtual.getIsbn().equals(isbn)){
+                this.catalogo.remove(i);
+            }
+        }
+    }
+
+    public ArrayList<Livro> buscar_por_titulo(titulo){
+        for(int i = 0; i < this.catalogo.size(); i++){
+            Livro livroAtual = this.catalogo.get(i);
+            
         }
     }
 }
+
+// 
--- JavaWorkSpace/Biblioteca/src/Livro.java
+++ JavaWorkSpace/Biblioteca/src/Livro.java
@@ -5,57 +5,78 @@ public class Livro {
     private boolean emprestado;
 
     public String getTitulo() {
-        return titulo;
+        return this.titulo;
     }
-    public void setTitulo(String titulo) {
+    private void setTitulo(String titulo) {
         this.titulo = titulo;
     }
     public String getAutor() {
-        return autor;
+        return this.autor;
     }
-    public void setAutor(String autor) {
+    private void setAutor(String autor) {
         this.autor = autor;
     }
     public String getIsbn() {
-        return isbn;
+        return this.isbn;
     }
-    public void setIsbn(String isbn) {
+    private void setIsbn(String isbn) {
         this.isbn = isbn;
     }
     public boolean isEmprestado() {
-        return emprestado;
+        return this.emprestado;
     }
-    public void setEmprestado(boolean emprestado) {
+    private void setEmprestado(boolean emprestado) {
         this.emprestado = emprestado;
     }
-    Livro(String titulo, String autor, String isbn ){
+    public Livro(String titulo, String autor, String isbn ){ //
       this.emprestado = false;
       this.titulo = titulo;
       this.autor = autor;
       this.isbn = isbn;
 
     }
+      public Livro(String titulo, String autor ){
+      this.emprestado = false;
+      this.titulo = titulo;
+      this.autor = autor;
+      this.isbn = "";
+    }
+
     public boolean reservar(){
-        if (this.emprestado == false){
-            this.emprestado = true;
-            System.out.println("Livro reservado");
-            return true;
+        if (this.isEmprestado()){
+            System.out.println("Livro ja esta emprestado");
+            return false;
         }
         else{
-            return false;
-            
+            System.out.println("Reserva realizada!");
+            this.setEmprestado(true);
+            return true;
         }
     }
 
-    public void devolver(){
-        this.emprestado = false;
+    public boolean devolver(){
+        if (this.isEmprestado()){
+            System.out.println("devolução realizada! ");
+            this.setEmprestado(false);
+            return true;
+        }
+        else {
+            System.out.println("livro esta na biblioteca!");
+            return false;
+        }
+        
     }
     
     public String exibir_info(){
-        return "Título: " + this.titulo + "\n" +
-               "Autor: " + this.autor + "\n" +
-               "ISBN: " + this.isbn + "\n" +
-               "Emprestado: " + (this.emprestado ? "Sim" : "Não");
+        String informacoes =  "Título: " + this.getTitulo() + "\n" +
+               "Autor: " + this.getAutor() + "\n" +
+               "Emprestado: " + (this.isEmprestado() ? "O livro esta emprestado" : "Disponivel");
+
+        if (this.getIsbn() != ""){
+            System.out.println("Título: " + this.getTitulo() + "\n" +
+               "Autor: " + this.getAutor() + "\n" +
+               "Emprestado: " + (this.isEmprestado() ? "O livro esta emprestado" : "Disponivel");
+        }
     }
     
 }
